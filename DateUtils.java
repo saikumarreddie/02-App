23b4f6b7561dc40a9a3befcefc32cc86bb93e747
saikumarreddie <saikumarreddybandi6@gmail.com>
2021-03-05 11:46:49 +0530

    HIS-301:Related changes

--- DateUtils.java
+++ DateUtils.java
@@ -1,9 +1,19 @@
 public class DateUtils{
+	priavte Map<String,Object> cache = new HashMap<>();
 
 
 	public void convertToDate(){
 
 	int a =10;
+
+
+	}
+
+	//HIS-301 changes
+	public void loadDataToCache(){
+
+	// logic goes here
+
 
 
 	}
